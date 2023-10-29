503b8b829b55e60b32868cded4b506731d1c2854
chiara <chiara.lazaro@utec.edu.pe>
2023-10-29 13:24:19 -0500

    owo

--- demo/src/main/java/com/example/demo/Application/CourseAssessmentDetailsController.java
+++ demo/src/main/java/com/example/demo/Application/CourseAssessmentDetailsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,4 +46,15 @@ public class CourseAssessmentDetailsController {
              new ResponseEntity<>(HttpStatus.OK);
         
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CourseAssessmentDetails> updateCourse(@PathVariable Long id, @RequestBody CourseAssessmentDetails course) {
+        CourseAssessmentDetails updatedCourse = courseAssessmentDetailsService.updateCourse(id, course);
+        
+        if (updatedCourse != null) {
+            return new ResponseEntity<>(updatedCourse, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }    
 }
